786df450de7cba9720c411b3b13d539883862168
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-06-16 22:04:45 +0300

    Added UserDetails

--- src/main/java/com/example/cinema/controller/UserController.java
+++ src/main/java/com/example/cinema/controller/UserController.java
@@ -1,13 +1,21 @@
 package com.example.cinema.controller;
 
+import com.example.cinema.dto.UserRegistrationDto;
+import com.example.cinema.entity.user.Telephone;
 import com.example.cinema.entity.user.User;
 import com.example.cinema.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -16,7 +24,35 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<User> registration(@RequestBody User user){
-        return ResponseEntity.ok(userService.save(user));
+    public ResponseEntity<User> registration(@RequestBody @Valid UserRegistrationDto userDto,
+                                             BindingResult bindingResult){
+        Map<String, String> errors = checkErrors(bindingResult);
+        if (errors.isEmpty()) {
+            User user = User.builder()
+                    .firstName(userDto.getFirstName())
+                    .lastName(userDto.getLastName())
+                    .username(userDto.getUsername())
+                    .password(userDto.getPassword())
+                    .email(userDto.getEmail())
+                    .age(userDto.getAge())
+                    .telephone(Telephone.builder()
+                            .code(userDto.getCode())
+                            .number(userDto.getNumber())
+                            .build())
+                    .build();
+            return ResponseEntity.ok(userService.save(user));
+        }
+           return ResponseEntity.badRequest().build();
+    }
+
+
+    private Map<String, String> checkErrors(BindingResult bindingResult){
+        Map<String, String> errors = new HashMap<>();
+        if (bindingResult.hasErrors()) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()){
+                errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+            }
+        }
+        return errors;
     }
 }
--- src/main/java/com/example/cinema/entity/user/User.java
+++ src/main/java/com/example/cinema/entity/user/User.java
@@ -3,14 +3,17 @@ package com.example.cinema.entity.user;
 import com.example.cinema.entity.AbstractEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Set;
 
 @Entity
 @Table(name = "users")
 @Setter @Getter
 @Builder @AllArgsConstructor @NoArgsConstructor
-public class User extends AbstractEntity {
+public class User extends AbstractEntity implements UserDetails {
     private String firstName;
     private String lastName;
     private String username;
@@ -24,4 +27,29 @@ public class User extends AbstractEntity {
     @ElementCollection
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
