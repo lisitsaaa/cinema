93f7473102cf548ad81417e579d8d3e78e8eac27
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-06-16 16:20:59 +0300

    Created classes - Movie and MovieSession

---
+++ src/main/java/com/example/cinema/entity/cinema/MovieSession.java
@@ -0,0 +1,22 @@
+package com.example.cinema.entity.cinema;
+
+import com.example.cinema.entity.AbstractEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Setter @Getter
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class MovieSession extends AbstractEntity {
+    private LocalDate date;
+    private LocalDate startedTime;
+
+    @OneToOne
+    private Movie movie;
+
+    @OneToOne
+    private Hall hall;
+}
