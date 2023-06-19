903ae3005d8b86b74b25abf18f5a3cea0bc37e29
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-06-19 19:15:52 +0300

    Added a method save in the class UserService

--- src/main/java/com/example/cinema/service/UserService.java
+++ src/main/java/com/example/cinema/service/UserService.java
@@ -1,21 +1,30 @@
 package com.example.cinema.service;
 
+import com.example.cinema.entity.user.Role;
 import com.example.cinema.entity.user.User;
 import com.example.cinema.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     public User save(User user){
+        user.setRoles(Set.of(Role.USER));
+        user.setPassword(passwordEncoder().encode(user.getPassword()));
         return userRepository.save(user);
     }
 
