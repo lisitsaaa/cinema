6b79b36ace6e791863e8f69c479281d33d6fd491
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-06-19 18:23:15 +0300

    Added security for registration

---
+++ src/main/java/com/example/cinema/configuration/SecurityConfiguration.java
@@ -0,0 +1,62 @@
+package com.example.cinema.configuration;
+
+import com.example.cinema.security.JWTTokenFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    private final JWTTokenFilter jwtTokenFilter;
+
+//    private static final String ADMIN_ENDPOINT = "/api/admin/**";
+    private static final String LOGIN_ENDPOINT = "/user/login";
+    private static final String REG_ENDPOINT = "/user";
+
+    private static final String[] PUBLIC_URLS = {
+            "/v2/api-docs",
+            "/swagger-ui/index.html",
+            "/swagger-resources/**",
+            "configuration/**",
+            "webjars/**",
+            "/*.html",
+            "/**/*.html",
+            "/**/*.css",
+            "/**/*.js"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers(LOGIN_ENDPOINT).permitAll()
+                .antMatchers(REG_ENDPOINT).permitAll()
+//                .antMatchers(ADMIN_ENDPOINT).hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.GET, PUBLIC_URLS).permitAll()
+                .antMatchers("/db/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        http
+                .headers().frameOptions().sameOrigin();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
