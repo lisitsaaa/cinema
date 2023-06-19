370e2944aa9b822c609abc52839ab711933123fd
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-06-19 19:13:22 +0300

    Created registration

--- src/main/java/com/example/cinema/controller/UserController.java
+++ src/main/java/com/example/cinema/controller/UserController.java
@@ -4,16 +4,13 @@ import com.example.cinema.dto.UserRegistrationDto;
 import com.example.cinema.entity.user.Telephone;
 import com.example.cinema.entity.user.User;
 import com.example.cinema.service.UserService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,33 +23,36 @@ public class UserController {
     @PostMapping
     public ResponseEntity<User> registration(@RequestBody @Valid UserRegistrationDto userDto,
                                              BindingResult bindingResult){
-        Map<String, String> errors = checkErrors(bindingResult);
-        if (errors.isEmpty()) {
-            User user = User.builder()
-                    .firstName(userDto.getFirstName())
-                    .lastName(userDto.getLastName())
-                    .username(userDto.getUsername())
-                    .password(userDto.getPassword())
-                    .email(userDto.getEmail())
-                    .age(userDto.getAge())
-                    .telephone(Telephone.builder()
-                            .code(userDto.getCode())
-                            .number(userDto.getNumber())
-                            .build())
-                    .build();
-            return ResponseEntity.ok(userService.save(user));
+        if (!getValidationResult(bindingResult)) {
+            return ResponseEntity.badRequest().build();
         }
-           return ResponseEntity.badRequest().build();
+        return ResponseEntity.ok(userService.save(buildRegistrationUser(userDto)));
     }
 
-
-    private Map<String, String> checkErrors(BindingResult bindingResult){
-        Map<String, String> errors = new HashMap<>();
+    private boolean getValidationResult(BindingResult bindingResult){
         if (bindingResult.hasErrors()) {
-            for (FieldError fieldError : bindingResult.getFieldErrors()){
+            Map<String, String > errors = new HashMap<>();
+            for(FieldError fieldError : bindingResult.getFieldErrors()){
                 errors.put(fieldError.getField(), fieldError.getDefaultMessage());
             }
+            System.out.println(errors);
+            return false;
         }
-        return errors;
+        return true;
+    }
+
+    private User buildRegistrationUser(UserRegistrationDto userDto){
+        return User.builder()
+                .firstName(userDto.getFirstName())
+                .lastName(userDto.getLastName())
+                .username(userDto.getUsername())
+                .password(userDto.getPassword())
+                .email(userDto.getEmail())
+                .age(userDto.getAge())
+                .telephone(Telephone.builder()
+                        .code(userDto.getCode())
+                        .number(userDto.getNumber())
+                        .build())
+                .build();
     }
 }
