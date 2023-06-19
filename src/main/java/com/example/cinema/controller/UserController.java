d53a396b234b6f3b1cf661d49e34f21fc66a10ee
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-06-19 19:50:30 +0300

    Added a method login in the class UserController

--- src/main/java/com/example/cinema/controller/UserController.java
+++ src/main/java/com/example/cinema/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.example.cinema.controller;
 
+import com.example.cinema.dto.UserAuthorizationDto;
 import com.example.cinema.dto.UserRegistrationDto;
 import com.example.cinema.entity.user.Telephone;
 import com.example.cinema.entity.user.User;
+import com.example.cinema.security.JWTTokenProvider;
 import com.example.cinema.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,13 +23,32 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private JWTTokenProvider jwtTokenProvider;
+
+    @GetMapping
+    public String hello(){
+        return "hello";
+    }
+
     @PostMapping
-    public ResponseEntity<User> registration(@RequestBody @Valid UserRegistrationDto userDto,
+    public ResponseEntity<User> registration(@RequestBody @Valid UserRegistrationDto regDto,
                                              BindingResult bindingResult){
         if (!getValidationResult(bindingResult)) {
             return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.ok(userService.save(buildRegistrationUser(userDto)));
+        return ResponseEntity.ok(userService.save(buildRegistrationUser(regDto)));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> authorization(@RequestBody @Valid UserAuthorizationDto authDto,
+                                              BindingResult bindingResult){
+        if (getValidationResult(bindingResult)) {
+            User login = userService.login(buildAuthorizationUser(authDto));
+            String token = jwtTokenProvider.generateToken(login.getUsername(), login.getRoles());
+            return ResponseEntity.ok(token);
+        }
+        return ResponseEntity.badRequest().body("Invalid information");
     }
 
     private boolean getValidationResult(BindingResult bindingResult){
@@ -41,17 +63,24 @@ public class UserController {
         return true;
     }
 
-    private User buildRegistrationUser(UserRegistrationDto userDto){
+    private User buildAuthorizationUser(UserAuthorizationDto authDto){
         return User.builder()
-                .firstName(userDto.getFirstName())
-                .lastName(userDto.getLastName())
-                .username(userDto.getUsername())
-                .password(userDto.getPassword())
-                .email(userDto.getEmail())
-                .age(userDto.getAge())
+                .username(authDto.getUsername())
+                .password(authDto.getPassword())
+                .build();
+    }
+
+    private User buildRegistrationUser(UserRegistrationDto regDto){
+        return User.builder()
+                .firstName(regDto.getFirstName())
+                .lastName(regDto.getLastName())
+                .username(regDto.getUsername())
+                .password(regDto.getPassword())
+                .email(regDto.getEmail())
+                .age(regDto.getAge())
                 .telephone(Telephone.builder()
-                        .code(userDto.getCode())
-                        .number(userDto.getNumber())
+                        .code(regDto.getCode())
+                        .number(regDto.getNumber())
                         .build())
                 .build();
     }
