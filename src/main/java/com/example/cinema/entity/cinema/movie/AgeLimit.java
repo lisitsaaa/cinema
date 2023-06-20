11232958ecdf0ca898785e8591fa6ade0c05b299
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-06-20 17:32:59 +0300

    Created enums

---
+++ src/main/java/com/example/cinema/entity/cinema/movie/AgeLimit.java
@@ -0,0 +1,5 @@
+package com.example.cinema.entity.cinema.movie;
+
+public enum AgeLimit {
+    ZERO_PLUS, SIX_PLUS, TWELVE_PLUS, SIXTEEN_PLUS, EIGHTEEN_PLUS
+}
---
+++ src/main/java/com/example/cinema/entity/cinema/movie/MovieType.java
@@ -0,0 +1,5 @@
+package com.example.cinema.entity.cinema.movie;
+
+public enum MovieType {
+    TWO_D, THREE_D
+}
