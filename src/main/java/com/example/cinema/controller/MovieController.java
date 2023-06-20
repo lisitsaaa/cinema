6fa725a18aa40c2c4f8a8da973e09533ecf04fa5
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-06-20 20:28:24 +0300

    Added a method getValidationResult

--- src/main/java/com/example/cinema/controller/MovieController.java
+++ src/main/java/com/example/cinema/controller/MovieController.java
@@ -6,13 +6,15 @@ import com.example.cinema.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/movie")
@@ -22,12 +24,26 @@ public class MovieController {
 
     @PostMapping
     public ResponseEntity<Movie> create(@RequestBody @Valid MovieCreatingDto movieCreatingDto,
-                                        BindingResult bindingResult){
+                                        BindingResult bindingResult) {
+        if (!getValidationResult(bindingResult)) {
+            return ResponseEntity.badRequest().build();
+        }
         return ResponseEntity.ok(movieService.save(buildMovie(movieCreatingDto)));
     }
 
+    private boolean getValidationResult(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                errors.put(fieldError.getCode(), fieldError.getDefaultMessage());
+            }
+            System.out.println(errors);
+            return false;
+        }
+        return true;
+    }
 
-    private Movie buildMovie(MovieCreatingDto dto){
+    private Movie buildMovie(MovieCreatingDto dto) {
         return Movie.builder()
                 .name(dto.getName())
                 .image(dto.getImage())
@@ -35,8 +51,8 @@ public class MovieController {
                 .description(dto.getDescription())
                 .releaseYear(dto.getReleaseYear())
                 .ageLimit(dto.getAgeLimit())
-                .genres(List.of(dto.getGenre()))
-                .types(dto.getMovieType())
+                .genres(dto.getGenres())
+                .type(dto.getMovieType())
                 .build();
     }
 }
