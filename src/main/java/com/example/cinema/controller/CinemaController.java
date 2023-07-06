340455eddf4c016bf215be4794f26e7506fd98a6
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-07-06 22:01:25 +0300

    Added a method - findById

--- src/main/java/com/example/cinema/controller/CinemaController.java
+++ src/main/java/com/example/cinema/controller/CinemaController.java
@@ -1,15 +1,12 @@
 package com.example.cinema.controller;
 
-import com.example.cinema.dto.CinemaCreatingDto;
+import com.example.cinema.dto.CinemaDto;
 import com.example.cinema.entity.cinema.Cinema;
 import com.example.cinema.service.CinemaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -22,19 +19,23 @@ public class CinemaController {
     private CinemaService cinemaService;
 
     @PostMapping
-    public ResponseEntity<Cinema> create(@RequestBody @Valid CinemaCreatingDto cinemaCreatingDto,
+    public ResponseEntity<Cinema> create(@RequestBody @Valid CinemaDto dto,
                                          BindingResult bindingResult){
         if(!getValidationResult(bindingResult)){
             return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.ok(cinemaService.save(buildCinema(cinemaCreatingDto)));
+        return ResponseEntity.ok(cinemaService.save(buildCinema(dto)));
     }
 
-    private Cinema buildCinema(CinemaCreatingDto dto){
+    private Cinema buildCinema(CinemaDto dto){
         return Cinema.builder()
                 .name(dto.getName())
                 .city(dto.getCity())
-                .halls(dto.getHalls())
                 .build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cinema> findById(@PathVariable long id){
+        return ResponseEntity.ok(cinemaService.findById(id));
     }
 }
