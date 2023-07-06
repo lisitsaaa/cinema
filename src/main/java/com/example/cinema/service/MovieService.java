4800d607ae442fbf918f61464d3fc34ec29e708c
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-07-06 21:53:25 +0300

    Added an annotation @Transactional in service classes

--- src/main/java/com/example/cinema/service/MovieService.java
+++ src/main/java/com/example/cinema/service/MovieService.java
@@ -4,8 +4,10 @@ import com.example.cinema.entity.cinema.movie.Movie;
 import com.example.cinema.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
--- src/main/java/com/example/cinema/service/SeatService.java
+++ src/main/java/com/example/cinema/service/SeatService.java
@@ -4,8 +4,10 @@ import com.example.cinema.entity.cinema.seat.Seat;
 import com.example.cinema.repository.SeatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class SeatService {
     @Autowired
     private SeatRepository seatRepository;
@@ -13,4 +15,8 @@ public class SeatService {
     public Seat save(Seat seat){
         return seatRepository.save(seat);
     }
+
+//    public Seat findHall(){
+//
+//    }
 }
