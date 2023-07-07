3b3498c1553bded4ab7f73ab5a7e324bb7a82197
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-07-07 22:56:49 +0300

    Added the method - findById

--- src/main/java/com/example/cinema/service/MovieSessionService.java
+++ src/main/java/com/example/cinema/service/MovieSessionService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 public class MovieSessionService {
@@ -14,5 +16,14 @@ public class MovieSessionService {
 
     public MovieSession save(MovieSession movieSession){
         return movieSessionRepository.save(movieSession);
+    }
+
+    @Transactional(readOnly = true)
+    public MovieSession findById(long id){
+        Optional<MovieSession> byId = movieSessionRepository.findById(id);
+        if (byId.isPresent()) {
+            return byId.get();
+        }
+        throw new RuntimeException("incorrect id");
     }
 }
