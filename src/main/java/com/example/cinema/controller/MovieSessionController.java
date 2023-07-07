4dc5e3a57382521f637d8697d0dd1dd4fcc129f3
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-07-07 22:55:43 +0300

    Added methods - create and findById

--- src/main/java/com/example/cinema/controller/MovieSessionController.java
+++ src/main/java/com/example/cinema/controller/MovieSessionController.java
@@ -1,9 +1,19 @@
 package com.example.cinema.controller;
 
+import com.example.cinema.dto.MovieSessionDto;
+import com.example.cinema.entity.cinema.MovieSession;
+import com.example.cinema.service.CinemaService;
+import com.example.cinema.service.HallService;
+import com.example.cinema.service.MovieService;
 import com.example.cinema.service.MovieSessionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import static com.example.cinema.controller.util.Validator.getValidationResult;
 
 @RestController
 @RequestMapping("/movie-session")
@@ -11,4 +21,38 @@ public class MovieSessionController {
     @Autowired
     private MovieSessionService movieSessionService;
 
+    @Autowired
+    private CinemaService cinemaService;
+
+    @Autowired
+    private HallService hallService;
+
+    @Autowired
+    private MovieService movieService;
+
+
+    @PostMapping
+    public ResponseEntity<MovieSession> create(@RequestBody @Valid MovieSessionDto dto,
+                                               BindingResult bindingResult) {
+        if (!getValidationResult(bindingResult)) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(movieSessionService.save(buildMovieSession(dto)));
+    }
+
+    private MovieSession buildMovieSession(MovieSessionDto dto) {
+        return MovieSession.builder()
+                .date(dto.getDate())
+                .startedTime(dto.getStartedTime())
+                .price(dto.getPrice())
+                .movie(movieService.findByName(dto.getMovieName()))
+                .cinema(cinemaService.findByName(dto.getCinemaName()))
+                .hall(hallService.findByName(dto.getHallName()))
+                .build();
+    }
+
+    @GetMapping("/{id}")
+    public MovieSession findById(@PathVariable long id) {
+        return movieSessionService.findById(id);
+    }
 }
