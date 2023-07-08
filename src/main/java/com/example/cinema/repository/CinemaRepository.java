ad8d0b2b76fca0eb607b7fe6362c14f0adac5215
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-07-09 01:08:15 +0300

    Added the annotation @Repository

--- src/main/java/com/example/cinema/repository/CinemaRepository.java
+++ src/main/java/com/example/cinema/repository/CinemaRepository.java
@@ -1,7 +1,6 @@
 package com.example.cinema.repository;
 
 import com.example.cinema.entity.cinema.Cinema;
-import com.example.cinema.entity.cinema.movie.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/example/cinema/repository/OrderRepository.java
+++ src/main/java/com/example/cinema/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.example.cinema.repository;
 
-import com.example.cinema.entity.order.Order;
+import com.example.cinema.entity.cinema.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
