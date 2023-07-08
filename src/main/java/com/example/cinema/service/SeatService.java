8d1e6d742a4cfced9a868bcb7f623845585a4fc8
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-07-09 01:09:28 +0300

    Added methods - findByHallAndRowAndSeat, update and findById

--- src/main/java/com/example/cinema/service/SeatService.java
+++ src/main/java/com/example/cinema/service/SeatService.java
@@ -1,10 +1,14 @@
 package com.example.cinema.service;
 
+import com.example.cinema.entity.cinema.Hall;
 import com.example.cinema.entity.cinema.seat.Seat;
+import com.example.cinema.entity.cinema.seat.SeatStatus;
 import com.example.cinema.repository.SeatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -14,5 +18,28 @@ public class SeatService {
 
     public Seat save(Seat seat){
         return seatRepository.save(seat);
+    }
+
+    @Transactional(readOnly = true)
+    public Seat findByHallAndRowAndSeat(Hall hall, int row, int seat){
+        Optional<Seat> byHallAndRowAndSeat = seatRepository.findByHallAndRowAndSeat(hall, row, seat);
+        if (byHallAndRowAndSeat.isPresent()) {
+            return byHallAndRowAndSeat.get();
+        }
+        throw new RuntimeException("incorrect data");
+    }
+
+    @Transactional(readOnly = true)
+    public Seat findById(long id){
+        Optional<Seat> byId = seatRepository.findById(id);
+        if (byId.isPresent()) {
+            return byId.get();
+        }
+        throw new RuntimeException("try again:)");
+    }
+
+    public Seat updateSeatStatus(long id, SeatStatus seatStatus){
+        seatRepository.update(id, seatStatus);
+        return findById(id);
     }
 }
