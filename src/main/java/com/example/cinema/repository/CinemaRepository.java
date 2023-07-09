c5e4fc61259798e40fb39b6dfcf0fa7304d13198
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-07-09 23:11:46 +0300

    Added a new method - findByCity

--- src/main/java/com/example/cinema/repository/CinemaRepository.java
+++ src/main/java/com/example/cinema/repository/CinemaRepository.java
@@ -4,9 +4,12 @@ import com.example.cinema.entity.cinema.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
     Optional<Cinema> findByName(String name);
+
+    List<Cinema> findByCity(String city);
 }
