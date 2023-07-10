d3ab9c4ad0397b119aeac18357c398b214f7b53a
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-07-10 10:07:32 +0300

    Added methods - update and findByCinema

--- src/main/java/com/example/cinema/repository/HallRepository.java
+++ src/main/java/com/example/cinema/repository/HallRepository.java
@@ -1,12 +1,22 @@
 package com.example.cinema.repository;
 
+import com.example.cinema.entity.cinema.Cinema;
 import com.example.cinema.entity.cinema.Hall;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface HallRepository extends JpaRepository<Hall, Long> {
     Optional<Hall> findByName(String name);
+
+    List<Hall> findByCinema(Cinema cinema);
+
+    @Modifying
+    @Query("update Hall h set h.name=: name where h.id=:id")
+    void update(long id, String name);
 }
