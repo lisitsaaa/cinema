93ef42a67d2e83bdf42fe5c96da63669ae4ccd7a
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-07-10 15:54:51 +0300

    Added a new class CinemaMapper

--- src/main/java/com/example/cinema/controller/CinemaController.java
+++ src/main/java/com/example/cinema/controller/CinemaController.java
@@ -2,7 +2,9 @@ package com.example.cinema.controller;
 
 import com.example.cinema.dto.CinemaDto;
 import com.example.cinema.entity.cinema.Cinema;
+import com.example.cinema.mapper.CinemaMapper;
 import com.example.cinema.service.CinemaService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -12,14 +14,15 @@ import javax.validation.Valid;
 import java.util.List;
 
 import static com.example.cinema.controller.util.Validator.getValidationResult;
+import static com.example.cinema.mapper.CinemaMapper.INSTANCE;
 import static org.springframework.http.ResponseEntity.badRequest;
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("/cinema")
+@RequiredArgsConstructor
 public class CinemaController {
-    @Autowired
-    private CinemaService cinemaService;
+    private final CinemaService cinemaService;
 
     @PostMapping
     public ResponseEntity<Cinema> create(@RequestBody @Valid CinemaDto dto,
@@ -27,14 +30,7 @@ public class CinemaController {
         if (!getValidationResult(bindingResult)) {
             return badRequest().build();
         }
-        return ok(cinemaService.save(buildCinema(dto)));
-    }
-
-    private Cinema buildCinema(CinemaDto dto) {
-        return Cinema.builder()
-                .name(dto.getName())
-                .city(dto.getCity())
-                .build();
+        return ok(cinemaService.save(INSTANCE.dtoToUser(dto)));
     }
 
     @GetMapping("/{id}")
