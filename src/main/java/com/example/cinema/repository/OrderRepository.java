62808341cce33b03cc95c7bfad3304e3e240919a
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-07-10 10:51:17 +0300

    Added methods - findAllByUser and update

--- src/main/java/com/example/cinema/repository/OrderRepository.java
+++ src/main/java/com/example/cinema/repository/OrderRepository.java
@@ -1,9 +1,20 @@
 package com.example.cinema.repository;
 
 import com.example.cinema.entity.cinema.Order;
+import com.example.cinema.entity.cinema.seat.Seat;
+import com.example.cinema.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findAllByUser(User user);
+
+    @Modifying
+    @Query("update Order o set o.seat=: seat where o.id=:id")
+    void update(long id, Seat seat);
 }
