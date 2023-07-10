e857aabefb67346c38a6af7deedf43c629bb3c5c
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2023-07-10 10:43:42 +0300

    Added methods - findByDate, findByCinema, findByMovie

--- src/main/java/com/example/cinema/repository/MovieSessionRepository.java
+++ src/main/java/com/example/cinema/repository/MovieSessionRepository.java
@@ -1,9 +1,31 @@
 package com.example.cinema.repository;
 
+import com.example.cinema.entity.cinema.Cinema;
+import com.example.cinema.entity.cinema.Hall;
 import com.example.cinema.entity.cinema.MovieSession;
+import com.example.cinema.entity.cinema.movie.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
 @Repository
 public interface MovieSessionRepository extends JpaRepository<MovieSession, Long> {
+    List<MovieSession> findByDate(LocalDate date);
+
+    List<MovieSession> findByCinema(Cinema cinema);
+
+    List<MovieSession> findByMovie(Movie movie);
+
+    @Modifying
+    @Query("update MovieSession ms set ms.date=: date, " +
+            "ms.startedTime=: startedTime, " +
+            "ms.price=: price, " +
+            "ms.hall=: hall " +
+            "where ms.id=: id")
+    void update(long id, LocalDate date, LocalTime startedTime, double price, Hall hall);
 }
